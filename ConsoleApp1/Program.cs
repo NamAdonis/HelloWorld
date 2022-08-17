53f876693706a22a07cfb20710f148c62b68a454
namna <namna@NAMNORMAL>
2022-08-17 21:34:28 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class mySweetProgram
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+//variable - stores some value
+//literal - actual value
+//identifier - name of variables
+//expression - evaluates to a value
+//operator - works on operands
+//operand - things operator works on
